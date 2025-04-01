82bb5196ef853edbe737b8de7a7ab19c5e7cd24b
wallace <wala-souza@hotmail.com>
2025-04-01 12:56:00 -0300

    Solucao do problema criando a classe exception e contador. Implementado o loop for e fazendo a contar a diferenca para imprimir a quantidade de interacoes. Jogando exception quando o segundo numero for menor que o primeiro

---
+++ desafio-contador/src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message); // Passa a mensagem para a classe Exception
+    }
+}
